2687ef1bec4b2391b24b7e7c6bbe4087f55fefe1
Shrish <b.shrish12@gmail.com>
2019-12-25 21:59:22 +1100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" class="bg-gray-200 w-full h-screen">
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Home,
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home flex justify-center flex-col items-center">
+      <img class="h-64 w-full object-cover" src="https://source.unsplash.com/random" alt />
+    <div class="flex flex-col items-center bg-gray-400 w-1/4 p-10 rounded-lg shadow-xl">
+      <img class="h-64 w-64 rounded-full" src="https://source.unsplash.com/random" alt />
+      <h5 class="text-2xl font-semibold">Image info</h5>
+      <p class="text-left mt-4">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Laboriosam fugiat doloremque id veritatis obcaecati incidunt reiciendis quo laborum qui quaerat fugit accusantium,
+        animi rerum esse a quidem voluptas ullam dolor!
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
+
+<style>
+</style>
